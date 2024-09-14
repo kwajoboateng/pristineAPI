c19f3fd0171d2b13689c781c741e73d21b4bdc6c
Kwajo Boateng <kwajoboateng31@gmail.com>
2024-09-14 19:52:07 -0400

    Starting to set up files

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import express from "express";
+import { VideoController } from "./controllers/videoController";
+import { UserController } from "./controllers/userController";
+
 const app = express();
 const port = 3000;
+
+app.get("/video", VideoController);
+app.get("/user", UserController);
 
 app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+	console.log(`Pristine app listening on port ${port}`);
 });
---
+++ controllers/videoController.js
@@ -0,0 +1,3 @@
+export const VideoController = () => {
+	//calls services
+};
